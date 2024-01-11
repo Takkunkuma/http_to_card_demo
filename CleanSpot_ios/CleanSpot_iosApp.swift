1b05139b8d87494f72b55a537cdf2661408afd15
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2024-01-10 21:28:59 -0800

    Initial Commit

---
+++ CleanSpot_ios/CleanSpot_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSpot_iosApp.swift
+//  CleanSpot_ios
+//
+//  Created by Tatsuo Kumamoto on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanSpot_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
